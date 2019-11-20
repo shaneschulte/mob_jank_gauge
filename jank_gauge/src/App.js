bfe5d3e34c11459c5a1e9b09a815324556f82705
Shane Schulte <shane.schulte@samsara.com>
2019-11-19 20:32:42 -0800

    done

--- jank_gauge/src/App.js
+++ jank_gauge/src/App.js
@@ -1,23 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactSpeedometer from "react-d3-speedometer";
 
+const prod_fetch = () => fetch("https://shaneschulte.com/mob_jank_gauge/jankness.txt");
+const test_fetch = async () => ({ json: async () => Math.random() * 1000});
+const getData = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? test_fetch : prod_fetch;
+
+const jank_text = (a) => {
+    if(a >= 0 && a < 200)
+    return "Iron-clad"
+    if(a >= 200 && a < 400)
+    return "Robust"
+    if(a >= 400 && a < 500)
+    return "Slightly Jank"
+    if(a >= 500 && a < 750)
+    return "Jank"
+    if(a >= 750 && a < 900)
+    return "Very Jank"
+    if(a >= 900 && a < 1000)
+    return "MAXIMUM Jank"
+    return "SamsaraDriver"
+}
 function App() {
+
+const [jank, setJank] = React.useState(0);
+const getJankness = () => {
+    getData()
+    .then(response => response.json()).then(result => setJank(result));
+}
+React.useEffect(() => {
+    const timer = setInterval(() => getJankness(), 1000);
+    return () => clearInterval(timer);
+});
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Mobile Jank Gauge
+          <div className="Inner-box">
+          <ReactSpeedometer
+              fluidWidth
+              customSegmentStops={[0, 500, 750, 900, 1000]}
+              segmentColors={["limegreen", "gold", "orange","firebrick"]}
+              labelFontSize={0}
+              valueTextFontSize={0}
+              value={jank}
+          />
+      </div>Current State: {jank_text(jank)}
       </header>
     </div>
   );
